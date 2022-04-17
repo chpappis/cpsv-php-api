799eb6debe4c86756edb6a66fa097bd31dcdd776
Christos Pappis <pappis@gmail.com>
2022-04-17 16:06:16 +0300

    added sparql endpoint to custom data provider

--- src/DataProvider/PublicOrganisationCollectionDataProvider.php
+++ src/DataProvider/PublicOrganisationCollectionDataProvider.php
@@ -15,18 +15,20 @@ final class PublicOrganisationCollectionDataProvider implements ContextAwareColl
 
     public function getCollection(string $resourceClass, string $operationName = null, array $context = []): iterable
     {
-        // $endpoint = 'http://data.dai.uom.gr:8890/sparql';
-        // $params = array('default-graph-uri' => 'http://data.dai.uom.gr:8890/CPSV-AP');
-        // $rdfnamespace = 'PREFIX cpsv: <http://purl.org/vocab/cpsv#> ';
-        // $rdfnamespace .= 'PREFIX dct: <http://purl.org/dc/terms/> ';
-        // $rdfnamespace .= 'PREFIX dcterms: <http://purl.org/dc/terms/> ';
-        // $rdfnamespace .= 'PREFIX cv: <http://data.europa.eu/m8g/> ';
-        // $params += array('query' => $rdfnamespace.'SELECT DISTINCT ?o ?name {?o a cpsv:PublicService. ?o dct:title ?name. }');
-        // $params += array('format' => 'json');
-        // $ch = curl_init();
-        // $url = $endpoint . '?' . http_build_query($params,"",null,PHP_QUERY_RFC1738);
-        // curl_setopt($ch, CURLOPT_URL, $url);
-        // $rdfresponse = curl_exec($ch);
+        $endpoint = 'http://data.dai.uom.gr:8890/sparql';
+        $params = array('default-graph-uri' => 'http://data.dai.uom.gr:8890/CPSV-AP');
+        $rdfnamespace = 'PREFIX cpsv: <http://purl.org/vocab/cpsv#> ';
+        $rdfnamespace .= 'PREFIX dct: <http://purl.org/dc/terms/> ';
+        $rdfnamespace .= 'PREFIX dcterms: <http://purl.org/dc/terms/> ';
+        $rdfnamespace .= 'PREFIX cv: <http://data.europa.eu/m8g/> ';
+        $params += array('query' => $rdfnamespace.'SELECT DISTINCT ?PO_uri ?preferredLabel WHERE {?PO_uri a cv:PublicOrganisation. ?PO_uri dct:title ?preferredLabel}');
+        $params += array('format' => 'json');
+        $ch = curl_init();
+        $url = $endpoint . '?' . http_build_query($params,"",null,PHP_QUERY_RFC1738);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $rdfresponse = curl_exec($ch);
+        $obj = json_decode($rdfresponse);
+        //results.bindings.preferredLabel
 
         $instance1 = new PublicOrganisation();
         $instance1->setPreferredLabel('test');
