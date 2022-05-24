c87b7a2b5ffeeeeea02d86d4c84fc0de97f4680c
Christos Pappis <pappis@gmail.com>
2022-05-24 20:40:46 +0300

    Add columns to Concept

--- src/Entity/Concept.php
+++ src/Entity/Concept.php
@@ -23,8 +23,38 @@ class Concept
     #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $label;
+
+    #[ORM\Column(type: 'string', length: 1024, nullable: true)]
+    private $definition;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(?string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getDefinition(): ?string
+    {
+        return $this->definition;
+    }
+
+    public function setDefinition(?string $definition): self
+    {
+        $this->definition = $definition;
+
+        return $this;
     }
 }
